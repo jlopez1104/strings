830a2ec59eaf80e34e75ef9d7a9bd41fc11f49eb
jlopez1104 <145376782+jlopez1104@users.noreply.github.com>
2023-11-06 17:59:26 +0000

    done

---
+++ strings.py
@@ -0,0 +1,23 @@
+author = 'Hemingway'
+
+#indexing
+print(author[0])
+print(author[1])
+print(author[2])
+#negative indexing
+print(author[-1])
+print(author[-2])
+print(author[-3])
+#slicing string
+#selecting a range of indexes
+print(author[2:5])
+print(author[2:-4])
+#concatenation
+print(author[0:2]) + (author[5:])
+x = 'John'
+greeting = input('Hello'+x+'how are you?')
+#formating
+#.lower
+#.upper
+#.capatilize
+#especially valuable in inputs
